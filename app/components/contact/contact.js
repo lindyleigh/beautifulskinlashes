230c745f9e69fc2a2d4035dabfb0318b9f0d9a3c
Lindy Crain <lindylcrain@gmail.com>
2016-09-26 13:52:17 -0600

    trying to add a google map

--- app/components/contact/contact.js
+++ app/components/contact/contact.js
@@ -9,6 +9,18 @@
     function ContactController() {
         var $ctrl = this;
 
+        function initMap() {
+        $ctrl.uluru = {lat: 43.5896444, lng: -116.2564094};
+        $ctrl.map = new google.maps.Map(angular.element(document.getElementById('map')), {
+          zoom: 4,
+          center: uluru
+        });
+        $ctrl.marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
     }
 
 } ());
