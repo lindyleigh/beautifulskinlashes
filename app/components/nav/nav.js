e8d26a77b97504971ee1e9291400701fc540e611
Lindy Crain <lindylcrain@gmail.com>
2016-10-03 15:15:31 -0600

    testing new hamburger menu

--- app/components/nav/nav.js
+++ app/components/nav/nav.js
@@ -9,6 +9,16 @@
     function NavController() {
         var $ctrl = this;
 
+        $ctrl.openMenu= function() {
+            angular.element(document.getElementById("hamburger").style.width = "250px");
+            angular.element(document.body.style.backgroundColor = "rgba(0,0,0,0.4)");
+        }
+
+        $ctrl.closeMenu= function() {
+            angular.element(document.getElementById("hamburger").style.width = "0");
+            angular.element(document.body.style.backgroundColor = "white");
+    }
+
     }
 
 
